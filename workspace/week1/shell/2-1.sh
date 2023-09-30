94072d1d1a8ea1b37157f0caac0aab2cd22a35f1
tongfakun <tfk12318.outlook.com>
2023-09-30 16:23:31 +0800

    增加图片，方便写readme

--- workspace/week1/shell/2-1.sh
+++ workspace/week1/shell/2-1.sh
@@ -3,5 +3,5 @@ mkdir /tmp/scripts
 cd /tmp/scripts
 cp  -r /etc/pam.d /tmp/scripts | mv pam.d test
 useradd "redhat"
-chown -R "redhat" test
-chmod -R g=---,o=--- test
+sudo chown -R "redhat" test
+sudo chmod -R g=---,o=--- test
