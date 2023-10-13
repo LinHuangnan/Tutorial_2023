d166ce6a57f43730b65733c10f3f9be0c5fe6b3e
tfk12318 <tfk12318@outlook.com>
2023-10-13 21:15:06 +0800

    readme.md

--- workspace/week3/opencv/src/test5.py
+++ workspace/week3/opencv/src/test5.py
@@ -4,7 +4,10 @@ import numpy as np
 img = cv2.imread('src/3.jpg')
 
 Kernel = np.ones((4,4), dtype=np.uint8)
-erosion = cv2.erode(img, Kernel, 1)
+cv2.imshow(img)
+dilate = cv2.dilate(img, Kernel, 1)
+cv2.imshow('dilate', dilate)
+erosion = cv2.erode(dilate, Kernel, 1)
 cv2.imshow('erosion', erosion)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
