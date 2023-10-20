f520e5300cd33a1461d3cee3aa35d7defe755996
LBM <3095088766@qq.com>
2023-10-20 14:34:11 +0800

    Changes to be committed: 	new file:   result.jpg 	new file:   workspace/image/week_4/image_5.png 	new file:   workspace/image/week_4/image_6.png 	new file:   workspace/image/week_4/image_7.png 	modified:   workspace/src_code/week_4/opencv_project_py/main.py 	new file:   workspace/src_code/week_4/opencv_project_py/readme.md 	new file:   workspace/src_code/week_4/opencv_project_py/test.py 	modified:   workspace/src_code/week_4/readme.md

--- workspace/src_code/week_4/opencv_project_py/main.py
+++ workspace/src_code/week_4/opencv_project_py/main.py
@@ -1,26 +1,29 @@
 import cv2
 import numpy as np
-
-video=cv2.VideoCapture("D:\\Tutorial_2023\\workspace\\src_code\\week_4\\opencv_project\\resource\\video2.mp4")
-frame_count=int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-frame_height=int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-frame_width=int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-matrix=np.zeros((frame_count,frame_height,frame_width,3),dtype=np.uint8)
-image_result=np.zeros((frame_height,frame_width,3),dtype=np.uint8)
+import time
+from rich.progress import track
+time_start=time.time()
+file_path="D:\\Tutorial_2023\\workspace\\src_code\\week_4\\opencv_project\\resource\\video3.mp4"
+video=cv2.VideoCapture(file_path)
 if video.isOpened:
-    for i in range(frame_count):
+    print("文件读取成功，正在处理......")
+    frame_count=int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    frame_height=int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_width=int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+    matrix=np.zeros((frame_count,frame_height,frame_width,3),dtype=np.uint8)
+    image_result=np.zeros((frame_height,frame_width,3),dtype=np.uint8)
+    for i in track(range(frame_count),description='[red]转换视频'):
         ret,frame=video.read()
         if frame is None:
             break
         if ret is True:
-            cv2.imshow("result",frame)
-            if cv2.waitKey(10) & 0xFF == 27 :
-                break
+            # cv2.imshow("result",frame)
+            # if cv2.waitKey(10) & 0xFF == 27 :
+            #     break
             matrix[i]=frame
 
 video.release()
-cv2.destroyAllWindows()
-for b in range(frame_height):
+for b in track(range(frame_height),description='[green]提取背景中'):
     for j in range(frame_width):
         for m in range(3):
             tmp1=matrix[:,b,j,m]
@@ -28,4 +31,9 @@ for b in range(frame_height):
             # result=list(tmp1).index(tmp2)
             image_result[b,j,m]=tmp2
 cv2.imshow("result",image_result)
+time_end=time.time()
+print("处理时间为：",time_end-time_start)
 cv2.waitKey(0)
+cv2.imwrite("./result.jpg",img=image_result)
+print("文件已保存在当前目录下")
+cv2.destroyAllWindows()
